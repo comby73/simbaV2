f54e99664765997a0307be951189fb7684fda293
Comby73 <comby73@msn.com>
2026-01-21 13:01:50 -0300

    fix: Restaurar imports de cors y helmet eliminados accidentalmente

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const { testConnection } = require('./config/database');
 
 // Importar rutas
